3782593b1994cb00890ad9218194a70b23dcf317
SarielAki <sariely@yandex.ru>
2019-12-20 16:41:06 +0300

    Now name field is unique

--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"username"`
+	Name     string `json:"username" gorm:"UNIQUE"`
 	Password string `json:"password"`
 }
